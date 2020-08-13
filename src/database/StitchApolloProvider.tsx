476b7a3dbbb829b52de837809d6f2fe26cb487a0
Dalmo Mendonça <dalmo3@gmail.com>
2020-08-13 17:45:32 +1200

    added stitch provider

---
+++ src/database/StitchApolloProvider.tsx
@@ -0,0 +1,70 @@
+import * as React from 'react';
+import { useStitchProvider, IStitchProvider } from './StitchProvider';
+import { persistCache } from 'apollo-cache-persist';
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  ApolloProvider,
+} from '@apollo/client';
+import { setContext } from 'apollo-link-context';
+import { PersistentStorage } from 'apollo-cache-persist/types';
+
+import { Plugins } from '@capacitor/core';
+
+const { Storage } = Plugins;
+
+// capacitor's Storage offer a different interface than the one peristCache wants, so we define translator functions
+const capacitorStorage = {
+  getItem: async (key: string) => (await Storage.get({ key })).value,
+  setItem: (key: string, data: any) =>
+    Storage.set({ key, value: data as string }),
+  // setItem: (key: string, data: PersistedData<{ value: string | null; }>) => Storage.set({ key, value: data as string }),
+  removeItem: (key: string) => Storage.remove({ key }),
+};
+
+const StitchApolloProvider: React.FC = ({ children }) => {
+  const { id, user } = useStitchProvider();
+  const cache = new InMemoryCache();
+  const [client, setClient] = React.useState(
+    createApolloClient(id, user, cache)
+  );
+  React.useEffect(() => {
+    const initApollo = async () => {
+      console.log('this runs?1');
+      await persistCache({
+        cache,
+        storage: capacitorStorage,
+      });
+      setClient(createApolloClient(id, user, cache));
+      console.log('this runs?2');
+    };
+    initApollo();
+  }, [id, user]);
+
+  return <ApolloProvider client={client}>{children}</ApolloProvider>;
+};
+export default StitchApolloProvider;
+
+// TODO: Implement createApolloClient()
+function createApolloClient(
+  realmAppId: string,
+  user: IStitchProvider['user'],
+  cache: InMemoryCache
+) {
+  const graphql_url = `https://realm.mongodb.com/api/client/v2.0/app/${realmAppId}/graphql`;
+  const httpLink = new HttpLink({ uri: graphql_url });
+  const authorizationHeaderLink = setContext(async (_, { headers }) => ({
+    headers: {
+      ...headers,
+      //@ts-ignore Type definitions for StitchUser are missing the ['auth'] property, that contain the accessToken
+      Authorization: `Bearer ${user.auth.activeUserAuthInfo.accessToken}`,
+    },
+  }));
+
+  return new ApolloClient({
+    link: authorizationHeaderLink.concat(httpLink),
+    // link: httpLink,
+    cache,
+  });
+}
---
+++ src/database/StitchProvider.tsx
@@ -0,0 +1,72 @@
+import * as React from 'react';
+import {
+  StitchUser,
+  StitchAppClient,
+  Stitch,
+  AnonymousCredential,
+} from 'mongodb-stitch-browser-sdk';
+
+const REALM_APP_ID = process.env.REACT_APP_REALM_APP_ID!;
+
+const stitchAppClientWeb: StitchAppClient = Stitch.initializeDefaultAppClient(
+  REALM_APP_ID
+);
+
+const StitchContext = React.createContext<IStitchProvider | void>(undefined);
+
+export interface IStitchProvider {
+  id: string;
+  user: StitchUser;
+  // logIn: (email: string, password: string) => Promise<void>;
+  // anonLogIn: () => Promise<void>;
+  // logOut: () => Promise<void>;
+  // registerUser(email: string, password: string): Promise<void>;
+}
+
+const StitchProvider: React.FC = ({ children }) => {
+  // Keep track of the current user in local state
+  // const appRef = React.useRef(realmWebApp);
+  // const [user, setUser] = React.useState(realmWebApp.currentUser);
+  const [user, setUser] = React.useState({} as IStitchProvider['user']);
+
+  // Anonymous login immediately upon app start
+  React.useEffect(() => {
+    const anonLogin = async () => {
+      const user = await stitchAppClientWeb.auth.loginWithCredential(
+        new AnonymousCredential()
+      );
+
+      setUser(user);
+    };
+    anonLogin();
+  }, []);
+
+  // Let logged in users log out
+  // const logOut = async () => {
+  //   // TODO: Log the current user out
+  //   await realmWebApp.logOut();
+  //   setUser(realmWebApp.currentUser);
+  // };
+
+  // Provide the current user and authentication methods to the wrapped tree
+  const context: IStitchProvider = {
+    id: REALM_APP_ID,
+    user,
+    // logIn,
+    // anonLogIn,
+    // logOut,
+    // registerUser,
+  };
+  return (
+    <StitchContext.Provider value={context}>{children}</StitchContext.Provider>
+  );
+};
+export default StitchProvider;
+
+export const useStitchProvider = (): IStitchProvider => {
+  const context = React.useContext(StitchContext);
+  if (!context) {
+    throw new Error('You must call useStitchApp() inside of a <StitchApp />.');
+  }
+  return context;
+};
