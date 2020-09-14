265647642aab6030658d4184145f74c20969444e
Dalmo Mendonça <dalmo3@gmail.com>
2020-09-14 17:56:08 +1200

    realm refresh current anon user on startup

--- src/database/RealmProvider.tsx
+++ src/database/RealmProvider.tsx
@@ -10,31 +10,32 @@ import * as Realm from 'realm-web';
 const REALM_APP_ID = process.env.REACT_APP_REALM_APP_ID!;
 
 const RealmContext = createContext<IRealmProvider | void>(undefined);
-const RealmAppClient = Realm.getApp( REALM_APP_ID ) as Realm.App;
+const RealmAppClient = Realm.getApp(REALM_APP_ID) as Realm.App;
 
 const RealmProvider: FC = ({ children }) => {
   const [user, setUser] = useState(RealmAppClient.currentUser!);
 
   // Anonymous login immediately upon app start
   useEffect(() => {
-    // console.log(RealmAppClient)
+    console.log(RealmAppClient);
     const anonLogin = async () => {
       const anonCredentials = Realm.Credentials.anonymous();
       await RealmAppClient.logIn(anonCredentials);
       setUser(RealmAppClient.currentUser!);
     };
-    if (!RealmAppClient.currentUser) anonLogin();
+    if (RealmAppClient.currentUser) {
+      RealmAppClient.currentUser.refreshAccessToken();
+    } else anonLogin();
     return () => {
-      console.log('effect cleanup ran')
+      console.log('effect cleanup ran');
       const logout = async () => {
-        console.log('user logged in is ', user)
+        console.log('user logged in is ', user);
         await RealmAppClient.currentUser?.logOut();
-        console.log('user logged out? ', user)
-      } 
-      logout()
+        console.log('user logged out? ', user);
+      };
+      logout();
     };
   }, []);
-
 
   // Provide the current user and authentication methods to the wrapped tree
   const context: IRealmProvider = {
