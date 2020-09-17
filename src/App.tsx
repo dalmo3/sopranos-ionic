9f6d307093b4647bc3afddf8defc5e0ff9d70cf9
Dalmo Mendonça <dalmo3@gmail.com>
2020-09-17 17:58:22 +1200

    added new routes (incomplete); cleaned old ones

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,6 @@ import {
 import { IonReactRouter } from '@ionic/react-router';
 import { ellipse, square, triangle } from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
-import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
 
 /* Core CSS required for Ionic components to work properly */
@@ -64,40 +63,41 @@ const App: React.FC = () => (
           <IonSplitPane contentId="main">
             <Menu />
             <IonPage id="main">
-              <IonFab vertical="top" horizontal="end">
+              {/* <IonFab vertical="top" horizontal="end">
                 <IonFabButton size="small">
-                    <IonMenuButton />
+                  <IonMenuButton />
                 </IonFabButton>
-              </IonFab>
+              </IonFab> */}
               <IonContent>
                 <Refresher />
-                <IonTabs>
-                  <IonRouterOutlet>
-                    <Route path="/tab1" component={Tab1} exact={true} />
-                    <Route path="/tab2" component={Tab2} exact={true} />
-                    <Route path="/tab3" component={Tab3} />
-                    <Route path="/settings" component={SettingsPage} />
-                    <Route
-                      path="/"
-                      render={() => <Redirect to="/tab1" />}
-                      exact={true}
-                    />
-                  </IonRouterOutlet>
-                  <IonTabBar slot="bottom">
-                    <IonTabButton tab="tab1" href="/tab1">
-                      <IonIcon icon={triangle} />
-                      <IonLabel>Tab 1</IonLabel>
-                    </IonTabButton>
-                    <IonTabButton tab="tab2" href="/tab2">
-                      <IonIcon icon={ellipse} />
-                      <IonLabel>Tab 2</IonLabel>
-                    </IonTabButton>
-                    <IonTabButton tab="tab3" href="/tab3">
-                      <IonIcon icon={square} />
-                      <IonLabel>Tab 3</IonLabel>
-                    </IonTabButton>
-                  </IonTabBar>
-                </IonTabs>
+                <IonRouterOutlet>
+                  <Route path="/settings" component={SettingsPage} />
+                  <Route path="/about" component={SettingsPage} />
+                  <Route path="/competitions" component={SettingsPage} />
+                  <Route path="/teams" component={SettingsPage} />
+                  <Route path="/help" component={SettingsPage} />
+                  <IonTabs>
+                    <IonRouterOutlet>
+                      <Route path="/tab1" component={Tab1} exact={true} />
+                      <Route path="/tab3" component={Tab3} />
+                      <Route
+                        path="/"
+                        render={() => <Redirect to="/tab1" />}
+                        exact={true}
+                      />
+                    </IonRouterOutlet>
+                    <IonTabBar slot="bottom">
+                      <IonTabButton tab="tab1" href="/tab1">
+                        <IonIcon icon={triangle} />
+                        <IonLabel>Tab 1</IonLabel>
+                      </IonTabButton>
+                      <IonTabButton tab="tab3" href="/tab3">
+                        <IonIcon icon={square} />
+                        <IonLabel>Tab 3</IonLabel>
+                      </IonTabButton>
+                    </IonTabBar>
+                  </IonTabs>
+                </IonRouterOutlet>
               </IonContent>
             </IonPage>
           </IonSplitPane>
