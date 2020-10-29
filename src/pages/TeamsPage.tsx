9c5d28f873bdc4c95612101a639cfca4563b3fc4
Dalmo Mendonça <dalmo3@gmail.com>
2020-10-29 15:32:52 +1300

    added header

--- src/pages/TeamsPage.tsx
+++ src/pages/TeamsPage.tsx
@@ -1,10 +1,20 @@
-import { IonPage } from '@ionic/react';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/react';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Header from '../components/Header';
 import TeamView from '../containers/TeamView';
 
 export default () => (
   <IonPage>
-    <Link to='/team/57953/fixtures'>TeamView</Link>
+    <Header title='Teams' />
+    <IonContent>
+      <Link to='/team/57953/fixtures'>TeamView</Link>
+    </IonContent>
   </IonPage>
 );
