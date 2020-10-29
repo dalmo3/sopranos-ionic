473efad47c1d2806facfdb2e3b2b9fa54f88bb75
Dalmo Mendonça <dalmo3@gmail.com>
2020-10-29 19:35:48 +1300

    added header and styling to comps page

--- src/pages/CompetitionsPage.tsx
+++ src/pages/CompetitionsPage.tsx
@@ -1,8 +1,19 @@
-import { IonPage } from '@ionic/react';
+import {
+  IonButton,
+  IonCard,
+  IonCardHeader,
+  IonContent,
+  IonItem,
+  IonList,
+  IonPage,
+  IonText,
+} from '@ionic/react';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Header from '../components/Header';
 import QueryHandlerContainer from '../containers/QueryHandlerContainer';
 import { useGetAllCompetitionsLiteQuery } from '../database/graphql-operations';
+import './CompetitionsPage.scss';
 
 const CompetitionsPage = () => {
   const queryResult = useGetAllCompetitionsLiteQuery();
@@ -11,13 +22,35 @@ const CompetitionsPage = () => {
   return (
     <QueryHandlerContainer queryResult={queryResult}>
       <IonPage>
-        {data?.competitions.map((comp) => {
-          return (
-            <Link to={`/competition/${comp?.Id}/fixtures`} key={comp?.Id}>
-              {comp?.Name}
-            </Link>
-          );
-        })}
+        <Header title='Competitions' />
+        <IonContent>
+          <IonList>
+            {data?.competitions.map((comp) => {
+              return (
+                // <IonItem>
+                // <Link to={`/competition/${comp?.Id}/fixtures`} key={comp?.Id}>
+                <IonButton
+                  color='light'
+                  expand='full'
+                  href={`/competition/${comp?.Id}/fixtures`}
+                  key={comp?.Id}
+                  // size='large'
+                  className={'competition-button'}>
+                  {/* <IonCard> */}
+                  {/* <IonCardHeader> */}
+                  {/* <IonText> */}
+                  {comp?.Name}
+                  {/* </IonText> */}
+
+                  {/* </IonCardHeader> */}
+                  {/* </IonCard> */}
+                </IonButton>
+                // </Link>
+                // </IonItem>
+              );
+            })}
+          </IonList>
+        </IonContent>
       </IonPage>
     </QueryHandlerContainer>
   );
